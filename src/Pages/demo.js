4dfd40a9ef471410e69fa43cffec77e0660a7ab7
Dart1516 <danvddpf1@gmail.com>
2024-05-17 12:50:05 -0300

    practicas demo

--- src/Pages/demo.js
+++ src/Pages/demo.js
@@ -1,13 +1,42 @@
 import logo from '../assets/images/logo.svg';
 import Header from "../Components/Header-NavMenu";
 import '../assets/styles/practicas.module.css';
+import React, { useState } from "react";
+import styles from "../assets/styles/practicas.module.css";
 
 function Demo() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className="App">
-      <header >
-        <Header/>
-      </header>
+    <header>
+      <Header />
+      {/* Icono del menú hamburguesa */}
+      <div className={styles["Hamburger-menu-icon"]} onClick={toggleMenu}>
+        <div className={menuOpen ? "open" : ""}></div>
+        <div className={menuOpen ? "open" : ""}></div>
+        <div className={menuOpen ? "open" : ""}></div>
+      </div>
+    </header>
+    {/* Contenido del menú */}
+    {menuOpen && (
+      <div className={styles["Mobile-menu"]}>
+        <ul>
+          <li>
+            <a href="#">Inicio</a>
+          </li>
+          <li>
+            <a href="#">Contacto</a>
+          </li>
+        </ul>
+      </div>
+    )}
+
+    
       <body className="App-body">
       <img src={logo} className="App-logo" alt="logo" />
        <h2>Demo</h2>
