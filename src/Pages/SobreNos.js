4cf6df7ac3d13e2575d9af1b7f247b7471049238
daysibel1175 <estergutierres1175@gmail.com>
2024-05-22 14:41:47 -0300

    update SobreNos

--- src/Pages/SobreNos.js
+++ src/Pages/SobreNos.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../Components/Header-NavMenu";
-import {Grid, Container, Typography, styled} from "@mui/material"
+import {Box, Grid, Container, Typography, styled} from "@mui/material"
 import imgComunidade from "../assets/images/voluntarios.jpg"
 import imgRS from "../assets/images/rs.jpg"
 
@@ -23,7 +23,7 @@ const About = () => {
   const StyledText = styled("div")(() => ({
       alignItems: "center",
       width:"100%",
-      padding:"10px"
+      padding:"5px"
   }))
   const StyledImg = styled("img")(() => ({
     width: "80%",
@@ -40,10 +40,10 @@ const About = () => {
                     <Container maxWidth="lg" gap={6}>
                     <Typography color="secondary.main" variant="h1" textAlign="center" pt={6}>Sobre Nós</Typography>
  {/* conteudo da primeira div ---------------------------------------------------- */}
-                    <Grid container spacing={2} gap={6} textAlign="center" justifyContent="center">
+                    <Grid container spacing={2} gap={1} textAlign="center" justifyContent="center">
                     <Grid item xs={12} md={7}>
                     <StyledText>
-                    <Typography variant="h6" pt={6} pb={4}  textAlign="start">
+                    <Typography  pt={3} pb={4}  textAlign="Start" justifyContent="center" variant="h6">
                                 A Comunidade Toti é um coletivo de profissionais imigrantes de tecnologia no Brasil, unidos pelo desejo de crescer profissionalmente e de apoiar uns aos outros. Nossa missão é oferecer um espaço de acolhimento, onde o conhecimento e as experiências se entrelaçam para criar oportunidades de desenvolvimento e integração.
                                       </Typography>
                                       </StyledText>
@@ -54,28 +54,26 @@ const About = () => {
 </Grid> 
                     </Grid>
   {/* conteudo da segunda div -------------------------------------- */}
-                    <Grid container spacing={2} gap={6} textAlign="center" justifyContent="center" direction={{ xs: 'column-reverse', md: 'row' }}>
+                    <Grid container spacing={2} gap={1} textAlign="center" justifyContent="center" direction={{ xs: 'column-reverse', md: 'row' }}>
                       {/* img da segunda div--------------------- */}
-                    <Grid item xs={12} md={4} display="flex" justifyContent="center" alignItems="center">
+                    <Grid item xs={12} md={4} display="flex" justifyContent="center" alignItems="center" >
                     <StyledImg  src={imgComunidade} />
                     </Grid>
                     <Grid item xs={12} md={7}>
                             <StyledText>
                           
-                            <Typography variant="h6" pt={6} pb={4}  textAlign="start">Em resposta aos recentes eventos no Rio Grande do Sul, nossa comunidade está mobilizada para fazer a diferença. Com empatia e solidariedade, lançamos esta plataforma para conectar voluntários e recursos, visando oferecer ajuda prática e esperança àqueles afetados pelas inundações.  O intuito dessa plataforma é oferecer apoio psicosocial às famílias afetadas pela tragédia no RS, disponibilizando serviços com psicólogos e educadores sociais. Esses profissionais estão prontos para atender e auxiliar no processo de recuperação emocional e social das pessoas impactadas, garantindo um suporte integral e humanizado.
+                            <Typography pt={3} pb={4}  textAlign="end" justifyContent="center" variant="h6">Em resposta a os recentes eventos no Rio Grande do Sul, nossa comunidade está mobilizada para fazer a diferença. Com empatia e solidariedade, lançamos esta plataforma para conectar voluntários e recursos, visando oferecer ajuda prática e esperança àqueles afetados pelas inundações.  O intuito dessa plataforma é oferecer apoio psicosocial às famílias afetadas pela tragédia no RS, disponibilizando serviços com psicólogos e educadores sociais. Esses profissionais estão prontos para atender e auxiliar no processo de recuperação emocional e social das pessoas impactadas, garantindo um suporte integral e humanizado.
                                       </Typography>
                                       </StyledText>
 </Grid></Grid>
-<Grid container spacing={2} gap={6} textAlign="center" justifyContent="center"  direction={{ xs: 'column-reverse', md: 'row' }}>
-
-                                      <Grid item xs={12} md={12} mb={4}>
-                                      <Typography variant="h6" width='100%' textAlign="center" pb={2} >
+<Box component="div" m={4}>
+<Typography  width='100%' textAlign="center"  pt={4} pb={2} variant="h6" >
                                       Convidamos profissionais de todas as áreas a se juntarem a este movimento humanitário, contribuindo com seu tempo e habilidades em apoio às vítimas das inundações no RS. Esperamos que muitos se juntem a nós neste esforço coletivo, pois juntos somos mais fortes. Todos juntos pelo Rio Grande do Sul.
                                       </Typography>
                                       <hr />  
-                                      </Grid>
                                     
-                                      </Grid>
+</Box>
+                                     
                                      
                                 
                     </Container>
