14865c4fb0c3c3b55a9446b77147d743bb7d914f
Victor Alcala <alcalavictor2001@gmail.com>
2024-05-22 05:09:27 -0400

    Update Home Responsive

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -2,7 +2,11 @@ import React from "react";
 import Header from "../Components/Header-NavMenu";
 import '../assets/styles/App.css';
 import '../assets/styles/HomePage.css'
-
+import psicologa from '../assets/images/psicologa.jpg'; 
+import educador from "../assets/images/educador-social.jpg";
+import lideres from "../assets/images/lideres.png";
+import voluntario from '../assets/images/voluntariado 1.jpeg';
+ 
 function Principal() {
   return (
     <div className="App">
@@ -25,10 +29,60 @@ function Principal() {
                 </div>
             </div>
           </div>
-      </body>
-      <footer>
+          <div className="voluntariado">
+          <div className='tipo-vo-texto'>
+              <h1>Como você pode ajudar?</h1>
+              <p>sua ajuda é crucial para fornecer apoio emergencial às comunidades afetadas, há várias maneiras pelas quais você pode contribuir:</p>
+            </div>
+            <div className="voluntariado-img"></div>
+          </div>
+          <div className="accordion">
+      <div className="accordion-container">
+        <div className="accordion-item">
+          <div className="accordion-content">
+            <h2>Voluntariado</h2>
+            <img src={psicologa} alt="Voluntariado Psicológico" />
+            <h2>Psicológico</h2>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <div className="accordion-content">
+            <h2>Educadores</h2>
+            <img src={educador} alt="Educadores Sociais" />
+            <h2>Sociais</h2>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <div className="accordion-content">
+            <h2>Líderes</h2>
+            <img src={lideres} alt="Líderes Comunitários" />
+            <h2>Comunitários</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="proceso-voluntariado">
+      <h1>processo de voluntariado</h1>
+      <div className="proceso-texto">
+        <h2>registro</h2>
+        <p>complete o <a href="./JoinUs">formulário</a> de inscrição com seus dados pessoais e experiência.</p>
+      </div>
+      <div className="proceso-texto"> 
+        <h2>capacitação</h2>
+        <p>participe de uma breve sessão de capacitação onde forneceremos todas as informações necessárias para sua função.</p>
+      </div>
+      <div className="proceso-texto"> 
+        <h2>atribuição</h2>
+        <p>você será designado para uma área específica com base em suas habilidades e disponibilidade três dias após o envio do formulário.</p>
+      </div>
+      <div className="proceso-texto"> 
+        <h2>ação</h2>
+        <p>comece a oferecer seu apoio às comunidades afetadas, fazendo uma diferença real e positiva.</p>
+      </div>
+      
 
-      </footer>
+    </div>
+      </body>
     </div>
   );
 }
