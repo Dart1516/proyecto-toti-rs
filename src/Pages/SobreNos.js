234fb34b2314858794863a33346ff30c9dda3359
daysibel1175 <estergutierres1175@gmail.com>
2024-06-06 18:39:21 -0300

    update sobrenos

--- src/Pages/SobreNos.js
+++ src/Pages/SobreNos.js
@@ -37,8 +37,8 @@ const About = () => {
     maxHeight:"400px"
   }));
   const StyledImgWithRadius = styled("img")(() => ({
-    width: "80%",
-    borderRadius: "50%",
+    width: "100%",
+    borderRadius: "5%",
     display: "flex",
     justifyContent: "center",
     alignItems: "center"
@@ -48,16 +48,17 @@ const About = () => {
       <>
        <Header/>
        <StyledAbout>
-                    <Container maxWidth="lg" padding="10rem">             
+                    <Container maxWidth="lg" >             
        
  {/* conteudo da primeira div ---------------------------------------------------- */}
                     <Grid container spacing={3} gap={0} justifyContent="center" alignItems="center" mt="20px"  mb="20px" display="flex" flexDirection="row" padding={3}>
                     <Grid item xs={12} md={6}>
                     <StyledText>
-                    <Typography  pt={3}  textAlign="Start" justifyContent="center" variant="h4" color="black">
+                    <Typography  pt={3}  textAlign="start" justifyContent="center" variant="h4" color="black" >
                                 Quem Somos?
                                       </Typography>
-                    <Typography   pb={4}  textAlign="Start" justifyContent="center" variant="body">
+                                     <hr style={{ border: "0",  margin: "0",height:"3px" ,maxWidth:'50%', marginBottom:"2rem", backgroundColor:"#FFB30B"}} />
+                    <Typography  textAlign="start" justifyContent="center" variant="body">
                                 A Comunidade Toti é um coletivo de profissionais imigrantes de tecnologia no Brasil, unidos pelo desejo de crescer profissionalmente e de apoiar uns aos outros. Nossa missão é oferecer um espaço de acolhimento, onde o conhecimento e as experiências se entrelaçam para criar oportunidades de desenvolvimento e integração.
                                       </Typography>
                                       </StyledText>
@@ -69,20 +70,24 @@ const About = () => {
                     </Grid>
                    
   {/* conteudo da segunda div -------------------------------------- */}
-                    <Grid container spacing={2} gap={0} textAlign="center" justifyContent="center" direction={{ xs: 'column-reverse', md: 'row' }} mt={2} padding={0}>
+                    <Grid container spacing={2} gap={0}  justifyContent="center" direction={{ xs: 'column-reverse', md: 'row' }} mt={2} padding={3}>
                       {/* img da segunda div--------------------- */}
-                    <Grid item xs={12} md={4} display="flex" justifyContent="center" alignItems="center" >
+                    <Grid item xs={12} md={6} display="flex" justifyContent="center" alignItems="center" >
                     <StyledImgWithRadius  src={imgComunidade} />
                     </Grid>
-                    <Grid item xs={12} md={7}>
-                            <StyledText>
-                          
-                            <Typography pt={3} pb={4}  textAlign="start" justifyContent="center" variant="h6">Em resposta a os recentes eventos no Rio Grande do Sul, nossa comunidade está mobilizada para fazer a diferença. Com empatia e solidariedade, lançamos esta plataforma para conectar voluntários e recursos, visando oferecer ajuda prática e esperança àqueles afetados pelas inundações.  O intuito dessa plataforma é oferecer apoio psicosocial às famílias afetadas pela tragédia no RS, disponibilizando serviços com psicólogos e educadores sociais. Esses profissionais estão prontos para atender e auxiliar no processo de recuperação emocional e social das pessoas impactadas, garantindo um suporte integral e humanizado.
+                    <Grid item xs={12} md={6}>
+                      <StyledText>
+                      <Typography  pt={3}  textAlign="start" justifyContent="center" variant="h4" color="black" >
+                  Nossa missão
+                                      </Typography>
+                                      <hr style={{ border: "0",  margin: "0",height:"3px" ,maxWidth:'50%', marginBottom:"2rem", backgroundColor:"#FFB30B"}} />
+                    <Typography  textAlign="start" justifyContent="center" variant="body">
+                    Em resposta a os recentes eventos no Rio Grande do Sul, nossa comunidade está mobilizada para fazer a diferença. Com empatia e solidariedade, lançamos esta plataforma para conectar voluntários e recursos, visando oferecer ajuda prática e esperança àqueles afetados pelas inundações.  O intuito dessa plataforma é oferecer apoio psicosocial às famílias afetadas pela tragédia no RS, disponibilizando serviços com psicólogos e educadores sociais. Esses profissionais estão prontos para atender e auxiliar no processo de recuperação emocional e social das pessoas impactadas, garantindo um suporte integral e humanizado.
                                       </Typography>
                                       </StyledText>
 </Grid></Grid>
 <Box component="div" m={4}>
-<Typography  width='100%' textAlign="center"  pt={4} pb={2} variant="h5" >
+<Typography  width='100%' textAlign="start"  pt={4} pb={2} variant="body" >
                                       Convidamos profissionais de todas as áreas a se juntarem a este movimento humanitário, contribuindo com seu tempo e habilidades em apoio às vítimas das inundações no RS. Esperamos que muitos se juntem a nós neste esforço coletivo, pois juntos somos mais fortes. Todos juntos pelo Rio Grande do Sul.
                                       </Typography>
                                       <hr />  
