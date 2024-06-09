1e1eeda403dab81f7b2460ac0dfed0eb3e3d11dc
Victor Alcala <alcalavictor2001@gmail.com>
2024-06-09 19:11:00 -0400

    Avance de lista de voluntarios

--- src/Pages/interfazLider.js
+++ src/Pages/interfazLider.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "../Components/Header-NavMenu";
 import '../assets/styles/App.css';
 import '../assets/styles/interfazLider.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faBuilding, faEllipsisH, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const professionals = [
   {
@@ -9,8 +11,9 @@ const professionals = [
     profession: "Psicólogo",
     availability: "Martes a las 8",
     state: "São Paulo",
+    city: "São Paulo",
     email: "ejemplo1@gmail.com",
-    phone: "123-456-7890",
+    phone: "(11) 1234-5678",
     image: "login/login1.png"
   },
   {
@@ -18,8 +21,9 @@ const professionals = [
     profession: "Educadora Social",
     availability: "Miércoles a las 10",
     state: "Rio de Janeiro",
+    city: "Rio de Janeiro",
     email: "ejemplo2@gmail.com",
-    phone: "987-654-3210",
+    phone: "(21) 98765-4321",
     image: "login/login1.png"
   },
   {
@@ -27,39 +31,99 @@ const professionals = [
     profession: "Psicólogo",
     availability: "Viernes a las 14",
     state: "Curitiba",
+    city: "Curitiba",
     email: "ejemplo3@gmail.com",
-    phone: "456-789-1230",
+    phone: "(41) 4567-8910",
+    image: "login/login1.png"
+  },
+  {
+    name: "Lic. Carlos Ruiz",
+    profession: "Psicólogo",
+    availability: "Viernes a las 14",
+    state: "Curitiba",
+    city: "Curitiba",
+    email: "ejemplo3@gmail.com",
+    phone: "(41) 4567-8910",
+    image: "login/login1.png"
+  },
+  {
+    name: "Lic. Carlos Ruiz",
+    profession: "Psicóloga",
+    availability: "Viernes a las 14",
+    state: "Curitiba",
+    city: "Curitiba",
+    email: "ejemplo3@gmail.com",
+    phone: "(41) 4567-8910",
     image: "login/login1.png"
   }
 ];
 
 function InterfazLider() {
+  // Calcular los conteos
+  const psicologosCount = professionals.filter(pro => pro.profession === "Psicólogo" || pro.profession === "Psicóloga").length;
+  const educadoresCount = professionals.filter(pro => pro.profession === "Educador Social" || pro.profession === "Educadora Social").length;
+  const otrosCount = professionals.filter(pro => pro.profession !== "Psicólogo" && pro.profession !== "Psicóloga" && pro.profession !== "Educador Social" && pro.profession !== "Educadora Social").length;
+  const totalCount = professionals.length;
+
   return (
     <div className="App">
       <header className="App-header">
         <Header />
       </header>
       <div className="App-body">
-        <h2>Bienvenido Líder</h2>
-        <h3>Psicólogos Disponibles</h3>
-        <div className="professional-list">
-          {professionals.map((professional, index) => (
-            <div key={index} className="professional-card">
-              <div className="left-section">
-                <img src={require(`../assets/images/${professional.image}`)} alt={professional.name} className="profile-image" />
-                <div className="professional-info">
-                  <h4>{professional.name}</h4>
-                  <p>{professional.profession}</p>
-                </div>
-              </div>
-              <div className="right-section">
-                <p><strong>Disponibilidad:</strong> {professional.availability}</p>
-                <p><strong>Estado:</strong> {professional.state}</p>
-                <p><strong>Email:</strong> {professional.email}</p>
-                <p><strong>Teléfono:</strong> {professional.phone}</p>
-              </div>
-            </div>
-          ))}
+        <h2 className="titulo-table">Cadastro especialidades</h2>
+        <div className="cards-container">
+          <div className="card">
+            <FontAwesomeIcon icon={faUser} className="icon-table"/>
+            <p>{psicologosCount}</p>
+            <h3>Psicólogos</h3>
+          </div>
+          <div className="card">
+            <FontAwesomeIcon icon={faBuilding} className="icon-table"/>
+            <p>{educadoresCount}</p>
+            <h3>Educadores</h3>
+          </div>
+          <div className="card">
+            <FontAwesomeIcon icon={faEllipsisH} className="icon-table"/>
+            <p>{otrosCount}</p>
+            <h3>Otros</h3>
+          </div>
+          <div className="card">
+            <FontAwesomeIcon icon={faPlus} className="icon-table"/>
+            <p>{totalCount}</p>
+            <h3>Total</h3>
+          </div>
+        </div>
+        <h2 className="titulo-table">Voluntários</h2>
+        <div className="table-container">
+          <table className="professional-table">
+            <thead>
+              <tr>
+                <th>Imagen</th>
+                <th>Nome</th>
+                <th>Especialidade</th>
+                <th>Contato</th>
+                <th>Estado/Cidade</th>
+              </tr>
+            </thead>
+            <tbody>
+              {professionals.map((professional, index) => (
+                <tr key={index}>
+                  <td>
+                    <img
+                      src={require(`../assets/images/${professional.image}`)}
+                      alt={professional.name}
+                      className="professional-image-table"
+                    />
+                  </td>
+                  <td>{professional.name}</td>
+                  <td>{professional.profession}</td>
+                  <td className="contato-table">{professional.email}<br/>{professional.phone}</td>
+                  <td>{professional.state}<br/>{professional.city}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
